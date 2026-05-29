54a99c81fa3f2dad6eebc53bcb35e842be9c7292
Cong1235 <ccon56852@gmail.com>
2026-05-29 14:29:24 +0700

    Add project files.

---
+++ CMS.Backend/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using CMS.data;
+using CMS.data.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMS.Backend.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        // "Tiêm" kết nối vào Controller
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            // Lấy dữ liệu THẬT từ bảng Categories trong SQL
+            var data = _context.Categories.ToList();
+            return View(data);
+        }
+
+    }
+}
+
---
+++ CMS.data/Entities/Customer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace CMS.data.Entities
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Fullname { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string? Phone { get; set; }
+        public string? Address { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public virtual ICollection<Order> Orders { get; set; }
+
+
+    }
+}
